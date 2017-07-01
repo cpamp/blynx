6a70e152b17fcbadd6b2eb682df34b380792f07e
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-07-01 02:00:37 -0500

    add: raised buttons

---
+++ packages/web-components/src/button/button.ts
@@ -0,0 +1,62 @@
+import { Component } from "@jable/browser-component/dist";
+import { JbRipple } from "../index";
+
+enum ButtonType {
+    Fab,
+    Raised,
+    Flat
+}
+
+
+export class JbButton {
+    private type: ButtonType;
+
+    constructor(private jbButton: HTMLElement) {
+        this.setType();
+        this.jbButton.setAttribute('jb-ripple', '');
+        this.setupButton();
+        new JbRipple(this.jbButton);
+    }
+
+    private setupButton() {
+        var hasPointer: boolean = (<any>window).PointerEvents != null;
+        this.jbButton.addEventListener(hasPointer ? 'pointerdown' : 'mousedown', () => {
+            this.jbButton.classList.add('jb-button-active');
+        });
+        document.addEventListener(hasPointer ? 'pointerup' : 'mouseup', () => {
+            this.jbButton.classList.remove('jb-button-active');
+        });
+        this.jbButton.addEventListener('focus', () => {
+            this.jbButton.classList.add('jb-button-focus');
+        });
+        this.jbButton.addEventListener('blur', () => {
+            this.jbButton.classList.remove('jb-button-focus');
+        });
+    }
+
+    private setType() {
+        switch (this.jbButton.getAttribute('jb-button').toLowerCase()) {
+            case ButtonType[ButtonType.Fab].toLowerCase():
+                this.type = ButtonType.Fab;
+                break;
+            case ButtonType[ButtonType.Flat].toLowerCase():
+                this.type = ButtonType.Flat;
+                break;
+            case ButtonType[ButtonType.Raised].toLowerCase():
+            default:
+                this.type = ButtonType.Raised;
+                break;
+        }
+        this.jbButton.setAttribute('jb-button', ButtonType[this.type].toLowerCase());
+    }
+}
+
+@Component({
+    selector: '[jb-button]',
+    styles: require('./button.scss')
+})
+export class JbButtonComponent extends JbButton {
+    constructor(el: HTMLElement) {
+        super(el);
+    }
+}
--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -1,6 +1,8 @@
 
-import { JbTextbox }from "./textbox/textbox";
-import { JbRipple } from "./ripple/ripple";
+import { JbTextbox, JbTextboxComponent } from "./textbox/textbox";
+import { JbRipple, JbRippleComponent } from "./ripple/ripple";
+import { JbButton, JbButtonComponent } from "./button/button";
 
-export { JbTextbox }
-export { JbRipple }
+export { JbTextboxComponent, JbTextbox }
+export { JbRippleComponent, JbRipple }
+export { JbButtonComponent, JbButton}
