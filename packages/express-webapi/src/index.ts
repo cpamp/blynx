fcdc32e84166ce0a55f2b7f84d3a11004e69e4f8
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-10-02 00:09:54 -0500

    fix(express-webapi): missing httpmethod export

--- packages/express-webapi/src/index.ts
+++ packages/express-webapi/src/index.ts
@@ -1,5 +1,6 @@
 import { Route } from "./route.decorator";
 import { Controller } from "./controller.decorator";
 import { WebApi } from "./webApi";
+import { HttpMethod } from "./httpMethod";
 
-export { Route, Controller, WebApi }
+export { Route, Controller, WebApi, HttpMethod }
