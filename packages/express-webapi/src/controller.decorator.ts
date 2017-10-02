f85d6fcf07a993d730ffbb1ac51fda73f4fa6705
Pamperin, Clint A <c.pamperin21@gmail.com>
2017-10-01 20:01:51 -0500

    fix: basePath on controller

--- packages/express-webapi/src/controller.decorator.ts
+++ packages/express-webapi/src/controller.decorator.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
 import { Router } from "./router";
 
-export function Controller() {
+export function Controller(basePath?: string) {
     return function(constructor: Function) {
-        Router.instance.registerController((<any>constructor));
+        Router.instance.registerController(<any>constructor, basePath);
     }
 }
