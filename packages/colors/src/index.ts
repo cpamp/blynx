68279a23e5ca17195678ee61648c9ec0f2b18c00
Clint Pamperin <clint.pamperin@zyquest.com>
2019-04-15 17:05:07 -0500

    add names, hex, reset

--- packages/colors/src/index.ts
+++ packages/colors/src/index.ts
@@ -7,11 +7,13 @@ const fg = (code: number) => 30 + code,
 
 //#region Styles
 
-export const italic = (text: string) => style(text, 3, 23)
+export const reset = () => style('', 0)
 
 export const bold = (text: string) => style(text, 1, 22)
 
 export const faint = (text: string) => style(text, 2, 22)
+
+export const italic = (text: string) => style(text, 3, 23)
 
 export const underline = (text: string) => style(text, 4, 24)
 
@@ -107,12 +109,68 @@ export const bbgWhite = (text: string) => style(text, bbg(7), bg(9))
 
 //#region custom colors
 
-export const color = (colorCode: number, text: string) => style(text, `${fg(8)};5;${colorCode}`, fg(9))
+export const color = (colorCode: number | ColorName | string, text: string) => {
+    if (typeof colorCode === 'string') return fromString(colorCode, text, false)
+    return style(text, `${fg(8)};5;${colorCode}`, fg(9))
+}
 
-export const bgColor = (colorCode: number, text: string) => style(text, `${bg(8)};5;${colorCode}`, bg(9))
+export const bgColor = (colorCode: number | ColorName | string, text: string) => {
+    if (typeof colorCode === 'string') return fromString(colorCode, text, true)
+    return style(text, `${bg(8)};5;${colorCode}`, bg(9))
+}
+
+function fromString(colorCode: ColorName | string, text: string, background: boolean) {
+    if (colorCode.indexOf('#') === 0) return fromHex(colorCode, text, background)
+    return fromName(colorCode as ColorName, text, background)
+}
 
 export const rgb = (red: number, green: number, blue: number, text: string) => style(text, `${fg(8)};2;${red};${green};${blue}`, fg(9))
 
 export const bgRgb = (red: number, green: number, blue: number, text: string) => style(text, `${bg(8)};2;${red};${green};${blue}`, bg(9))
+
+//#endregion
+
+//#region Hex
+
+function checkHex(hex: number) {
+    if (hex > 255 || hex < 0) throw new Error('Invalid hex value')
+}
+
+function fromHex(hex: string, text: string, background: boolean) {
+    hex = hex.slice(1)
+    let rgbVals: [number, number, number] = [0, 0, 0]
+    if (hex.length === 3) {
+        for (let i = 0; i < hex.length; i++) {
+            let val = hex[i]
+            let hexVal = parseInt(val + val, 16)
+            checkHex(hexVal)
+            rgbVals.push(hexVal)
+        }
+    } else if (hex.length === 6) {
+        for (let i = 1; i < hex.length; i += 2) {
+            let hexVal = parseInt(hex.slice(i - 1, i), 16)
+            checkHex(hexVal)
+            rgbVals.push(hexVal)
+        }
+    } else {
+        checkHex(-1)
+    }
+    const params: [number, number, number, string] = [rgbVals[0], rgbVals[1], rgbVals[2], text]
+    return background ? bgRgb.apply(null, params) : rgb.apply(null, params)
+}
+
+//#endregion
+
+//#region names
+
+import { ColorName, names, } from './names'
+
+function fromName(name: ColorName, text: string, background: boolean) {
+    name = name.toLowerCase() as ColorName
+    let rgbVals = names[name]
+    if (rgbVals == null) throw new Error('Invalid color name')
+    const params: [number, number, number, string] = [rgbVals[0], rgbVals[1], rgbVals[2], text]
+    return background ? bgRgb.apply(null, params) : rgb.apply(null, params)
+}
 
 //#endregion
